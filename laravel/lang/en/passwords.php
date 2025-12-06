720d2498085c7fed4cfa8a8c8187daa8541db547
DarijanVasatko <darijanko114@gmail.com>
2025-12-06 13:41:35 +0100

    napravio sam da se mail salje kada se hoće promjeniti lozinka

---
+++ laravel/lang/en/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Tvoja lozinka je resetirana.',
+    'sent' => 'Poslali smo ti link za resetiranje lozinke.',
+    'throttled' => 'Molimo pričekaj prije ponovnog pokušaja.',
+    'token' => 'Ovaj token za resetiranje lozinke nije valjan.',
+    'user' => "Ne možemo pronaći korisnika s tom email adresom.",
+];
